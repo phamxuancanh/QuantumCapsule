d7329863458ba98f107501e6bf21e818403d3fba
phamxuancanh <pxcpaze@gmail.com>
2024-09-20 00:25:39 +0700

    hot fix theory api

--- client/src/pages/learning/index.tsx
+++ client/src/pages/learning/index.tsx
@@ -1,11 +1,36 @@
-import React, { useEffect, useMemo, useState } from 'react';
-const Learning = () => {
-    return (
-        <div className='tw-flex tw-items-center tw-justify-center tw-bg-slate-50'>
-                CANH DEP TRAI
-        </div>
+import React, { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import { getTheoryById } from 'api/theory/theory.api';
+import { ITheory } from 'api/theory/theory.interface';
 
-    )
+const Learning = () => {
+  const [theory, setTheory] = useState<ITheory | null>(null);
+  const location = useLocation();
+
+  useEffect(() => {
+    const queryParams = new URLSearchParams(location.search);
+    const theoryId = queryParams.get('theoryId');
+
+    if (theoryId) {
+      fetchTheory(theoryId);
+    }
+  }, [location]);
+
+  const fetchTheory = async (id: string) => {
+    try {
+      const response = await getTheoryById(id);
+      setTheory(response.data.theory);
+      console.log('Theory Name:', response.data);
+    } catch (error) {
+      console.error('Error fetching theory:', error);
+    }
+  };
+
+  return (
+    <div className='tw-flex tw-items-center tw-justify-center tw-bg-slate-50'>
+      {theory ? theory.name : 'Loading...'}
+    </div>
+  );
 };
 
 export default Learning;
--- server/routes/theory.js
+++ server/routes/theory.js
@@ -8,5 +8,6 @@ router.get('/', verifyAccessToken, theoryController.getListTheory)
 router.post('/', verifyAccessToken, theoryController.addTheory)
 router.put('/:id', verifyAccessToken, theoryController.updateTheory)
 router.delete('/:id', verifyAccessToken, theoryController.deleteTheory)
+router.get('/:id', verifyAccessToken, theoryController.getTheoryById)
 router.get('/getTheoriesByLessonId/:lessonId', verifyAccessToken, theoryController.getTheoriesByLessonId)
 module.exports = router
