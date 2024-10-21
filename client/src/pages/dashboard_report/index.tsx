965b9a7ea0dfe387be2697efaaf0eb346f9be7c3
phamxuancanh <pxcpaze@gmail.com>
2024-10-21 23:33:24 +0700

    nav bar

---
+++ client/src/pages/dashboard_report/index.tsx
@@ -0,0 +1,13 @@
+import React, { useEffect, useMemo, useState } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
+const Home = () => {
+
+    return (
+        <div className='tw-text-lg tw-bg-slate-50 tw-flex tw-items-center tw-justify-center tw-min-h-screen'>
+            PHAM XUAN CANH
+        </div>
+
+    )
+};
+
+export default Home;
