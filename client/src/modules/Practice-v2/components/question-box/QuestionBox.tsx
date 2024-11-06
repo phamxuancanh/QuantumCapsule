61cfab6d900e50bcab0fd2d21b9fd530a8ad4229
Phạm Hà Nam 20069861 <98084540+pnam55588@users.noreply.github.com>
2024-11-06 10:15:37 +0700

    practice v2

--- client/src/modules/Practice-v2/components/question-box/QuestionBox.tsx
+++ client/src/modules/Practice-v2/components/question-box/QuestionBox.tsx
@@ -14,7 +14,9 @@ import QuestionV1 from "QCComponents/questions/question-v1/QuestionV1"
 import QuestionV2 from "QCComponents/questions/question-v2/QuestionV2"
 import QuestionV3 from "QCComponents/questions/question-v3/QuestionV3"
 import SpeakerV1 from "QCComponents/speakers/speaker-v1/SpeakerV1"
-import React from "react"
+import React, { useCallback, useEffect, useRef, useState } from "react"
+import ReactCanvasConfetti from 'react-canvas-confetti';
+import Fireworks from "react-canvas-confetti/dist/presets/fireworks";
 
 interface IProps {
     isOpen?: boolean
@@ -28,6 +30,21 @@ const QuestionBox: React.FC<IProps> = (props) => {
     const {openResult, setOpenResult} = useOpenResult()
     const [isNextQuestion, setIsNextQuestion] = React.useState(false)
     const {actCongratulation,setActCongratulation} = useActCongratulation()
+    const confettiRef = useRef(null);
+    const correctSoundUrl = '/path/to/congrats.mp3';
+    const errorSoundUrl = '/path/to/error.mp3';
+    const [isFireworksRunning, setIsFireworksRunning] = useState(false);
+
+
+    useEffect(() => {
+        if (isFireworksRunning) {
+          const timer = setTimeout(() => {
+            setIsFireworksRunning(false); // Dừng pháo hoa sau 3 giây
+          }, 3000);
+          return () => clearTimeout(timer); // Xóa bộ đếm thời gian khi component unmount
+        }
+      }, [isFireworksRunning]);
+
     const handleAnswer = (yourAnswer: string) => {
         const newListAnswer = listAnswer.map((answer: IAnswer) => {
             if (answer.questionId === currentQuestion.id) {
@@ -52,7 +69,9 @@ const QuestionBox: React.FC<IProps> = (props) => {
             setIsNextQuestion(true)
             return
         }
-        
+        const yourAnswer = listAnswer.find((ans) => ans.questionId === currentQuestion.id)
+        playSound(yourAnswer?.isCorrect!)
+        setIsFireworksRunning(yourAnswer?.isCorrect!)
         setIsNextQuestion(false)
         const index = listQuestion.findIndex(
             (question: IQuestion) => question.id === currentQuestion.id,
@@ -77,6 +96,13 @@ const QuestionBox: React.FC<IProps> = (props) => {
         }
         return <></>
     }
+    const playSound = (isCorrect: boolean) => {
+        const soundUrl = isCorrect ? correctSoundUrl : errorSoundUrl;
+        const audio = new Audio(soundUrl);
+        audio.play();
+      };
+    
+     
     return (
         <Box display={props.isOpen ? "block" : "none"}>
             <SpeakerV1 text={currentQuestion?.content!} label="Đọc câu hỏi" autoSpeak />
@@ -89,6 +115,10 @@ const QuestionBox: React.FC<IProps> = (props) => {
             >
                 {isNextQuestion ?  'Câu tiếp theo' : 'xem kết quả'}
             </Button>
+            {/* <ReactCanvasConfetti refConfetti={makeConfetti} /> */}
+            {isFireworksRunning && (
+                <Fireworks autorun={{ speed: 3 }} style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%' }} />
+            )}
         </Box>
     )
 }
