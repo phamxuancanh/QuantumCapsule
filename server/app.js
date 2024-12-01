e3d56f8eeab1ef5eb662aba7747e3ff80bc24bb8
phamxuancanh <pxcpaze@gmail.com>
2024-12-01 23:38:01 +0700

    z

---
+++ server/app.js
@@ -0,0 +1,97 @@
+const express = require('express')
+const morgan = require('morgan')
+const http = require('http')
+const socket = require('./socket')
+// const session = require('express-session')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const { sequelize } = require('./models')
+const seedDatabase = require('./seeds/index')
+const IndexRouter = require('./routes/index')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const socketEvents = require('./socketEvent')
+const app = express()
+const server = http.createServer(app)
+
+app.set('trust proxy', true)
+
+// app.use(session({
+//   secret: 'your_secret_key',
+//   resave: false,
+//   saveUninitialized: true
+// }))
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*') // Cho phép tất cả các nguồn
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE') // Các phương thức được phép
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,Authorization') // Các header được phép
+  res.setHeader('Access-Control-Allow-Credentials', true) // Cho phép gửi cookie/credentials
+  res.setHeader('Cross-Origin-Opener-Policy', 'same-origin')
+  res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp')
+  res.setHeader('Cross-Origin-Resource-Policy', 'same-origin')
+  res.setHeader('Cross-Origin-Opener-Policy', 'same-origin-allow-popups')
+
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200)
+  }
+  next()
+})
+
+// app.use(function (req, res, next) {
+//   const allowedOrigins = ['https://quantum-capsule.vercel.app', 'http://localhost:3000', 'http://3.27.217.226:3000'] // Thêm localhost
+//   const origin = req.headers.origin
+
+//   if (allowedOrigins.includes(origin)) {
+//     res.setHeader('Access-Control-Allow-Origin', origin)
+//   }
+
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+//   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,Authorization')
+//   res.setHeader('Access-Control-Allow-Credentials', true)
+//   res.setHeader('Cross-Origin-Opener-Policy', 'same-origin')
+//   res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp')
+//   res.setHeader('Cross-Origin-Resource-Policy', 'same-origin')
+//   res.setHeader('Cross-Origin-Opener-Policy', 'same-origin-allow-popups')
+
+//   if (req.method === 'OPTIONS') {
+//     return res.sendStatus(200)
+//   }
+//   next()
+// })
+
+app.use(cors({
+  // origin: ['https://quantum-capsule.vercel.app', 'http://localhost:3000', 'http://3.27.217.226:3000'], // Thêm localhost
+  origin: '*',
+  methods: 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
+  allowedHeaders: 'X-Requested-With,Content-Type,Authorization'
+  // credentials: true
+}))
+
+app.use(cookieParser())
+app.use(bodyParser.json({ limit: '100mb' }))
+app.use(bodyParser.urlencoded({ extended: true, limit: '100mb' }))
+app.use(morgan('combined'))
+app.use(express.json({ limit: '100mb' }))
+app.use(express.urlencoded({ extended: true, limit: '100mb' }))
+app.use('/static', express.static(path.join(__dirname, 'public')))
+app.use('/', IndexRouter)
+socket.init(server)
+socketEvents()
+async function startServer () {
+  try {
+    await sequelize.sync()
+    console.log('Database synchronized successfully')
+    await seedDatabase()
+    console.log('Data seeded successfully')
+
+    server.listen(process.env.PORT || 443, () => {
+      console.log('Server is running on port', process.env.PORT)
+    })
+  } catch (error) {
+    console.error('Error starting server:', error)
+  }
+}
+
+startServer()
+
+module.exports = { app }
--- server/socket.js
+++ server/socket.js
@@ -3,7 +3,7 @@ let io
 function init (server) {
   io = require('socket.io')(server, {
     cors: {
-      origin: 'https://quantum-capsule.vercel.app',
+      origin: ['https://quantum-capsule.vercel.app', 'http://localhost:3000', 'http://3.27.217.226:3000'], // Thêm localhost
       methods: ['GET', 'POST'],
       allowedHeaders: ['Content-Type', 'Authorization'],
       credentials: true
